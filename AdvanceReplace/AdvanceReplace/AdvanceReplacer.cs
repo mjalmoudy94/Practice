45f7b61f9e32678dbdd8c50628154a457ee0b358
mjalmoudy94 <49875446+mjalmoudy94@users.noreply.github.com>
2023-01-22 15:40:56 +0330

    Add files via upload

---
+++ AdvanceReplace/AdvanceReplace/AdvanceReplacer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalmoudy.String
+{
+    public static class AdvanceReplacer
+    {
+        public static (bool isSuccess, string result) ReplaceZero_WithStar_UsingArray(string input)
+        {
+            char[] charBox = new char[10] { '*', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            bool validation = true;
+            string result = "";
+
+            try
+            {
+                foreach (char character in input.ToArray())
+                {
+                    result += charBox[int.Parse(character.ToString())];
+                }
+            }
+            catch (Exception)
+            {
+                validation = false;
+                result = null;
+            }
+
+            return (validation, result);
+        }
+
+        public static (bool isSuccess, string result) ReplaceZero_WithStar_UsingDictionary(string input)
+        {
+            Dictionary<char, char> charBox = new Dictionary<char, char>();
+            charBox.Add('0', '*');
+            charBox.Add('1', '1');
+            charBox.Add('2', '2');
+            charBox.Add('3', '3');
+            charBox.Add('4', '4');
+            charBox.Add('5', '5');
+            charBox.Add('6', '6');
+            charBox.Add('7', '7');
+            charBox.Add('8', '8');
+            charBox.Add('9', '9');
+
+            bool validation = true;
+            string result = string.Empty;
+
+            try
+            {
+                List<char> reslitList = input.ToList().Select(x => charBox[x]).ToList();
+                result = string.Join("", reslitList);
+            }
+            catch (Exception)
+            {
+                validation = false;
+            }
+
+            return (validation, result);
+        }
+    }
+}
---
+++ AdvanceReplace/AdvanceReplace/App.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalmoudy.String
+{
+    class App
+    {
+        // از شرط فقط جهت نمایش  نتیجه استفاده شده
+        // ولیدیشن و جایگذاری بدون شرط پیاده سازی شدن
+        static void Main(string[] args)
+        {
+            string wrong_UserInput = "l4580G054Y";
+            string right_UserInput = "1458020547";
+
+
+            // Use Array To Replace And Validate
+            // Wrong User Input
+            var wrong_ReplacerResult_UsingArray = AdvanceReplacer.ReplaceZero_WithStar_UsingArray(wrong_UserInput);
+            if (wrong_ReplacerResult_UsingArray.isSuccess)
+            {
+                Console.WriteLine(wrong_ReplacerResult_UsingArray.result);
+            }
+            else
+            {
+                Console.WriteLine("Only Numbers Are Alowed");
+            }
+
+            // Right User Input
+            var right_ReplacerResult_UsingArray = AdvanceReplacer.ReplaceZero_WithStar_UsingArray(right_UserInput);
+            if (right_ReplacerResult_UsingArray.isSuccess)
+            {
+                Console.WriteLine(right_ReplacerResult_UsingArray.result);
+            }
+            else
+            {
+                Console.WriteLine("Only Numbers Are Alowed");
+            }
+
+
+            // Use Dictionary To Replace And Validate
+            // Wrong User Input
+            var wrong_ReplacerResult_UsingDictionary = AdvanceReplacer.ReplaceZero_WithStar_UsingDictionary(wrong_UserInput);
+            if (wrong_ReplacerResult_UsingDictionary.isSuccess)
+            {
+                Console.WriteLine(wrong_ReplacerResult_UsingDictionary.result);
+            }
+            else
+            {
+                Console.WriteLine("Only Numbers Are Alowed");
+            }
+
+            // Right User Input
+            var right_ReplacerResult_UsingDictionary = AdvanceReplacer.ReplaceZero_WithStar_UsingDictionary(right_UserInput);
+            if (right_ReplacerResult_UsingDictionary.isSuccess)
+            {
+                Console.WriteLine(right_ReplacerResult_UsingDictionary.result);
+            }
+            else
+            {
+                Console.WriteLine("Only Numbers Are Alowed");
+            }
+
+
+            // END
+        }
+    }
+}
